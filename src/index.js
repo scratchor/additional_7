d4dc1091822593f5460ff8fd2cec5ddd9fbce09c
Владислав Царапкин <scratchor@mail.ru>
2018-10-07 21:49:33 +0300

    change task 8

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ module.exports = function solveSudoku(matrix) {
           sum+=answer[i][j];
           sum1+=answer[j][i];
         }
-        if(num >= 20000) {
+        if(num >= 1000) {
             return answer;
         }
        if(sum !== 45 || sum1 !== 45) {
